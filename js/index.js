6f4bb630a6a1144296f593437162e7ec08c6d5e0
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-11-26 14:00:21 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,16 +1,6 @@
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
-xhr.responseType = 'json';
- 
-xhr.onload = function() {
-  console.log(xhr.response);
-};
- 
-xhr.onerror = function() {
-  console.log('Booo');
-};
- 
-xhr.send();
+fetch('https://api.github.com/repos/jquery/jquery/commits')
+  .then(res => res.json())
+  .then(json => console.log(json));
 
 
 const app = "I don't do much.";
