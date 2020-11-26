5f3a916bae3babb35c6a472b0f734706f0d385fd
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-11-26 14:33:21 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
+const token = '4090d01b2231718f97101cb785dae48e8ee3a78d';
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
+
+
 const app = "I don't do much.";
