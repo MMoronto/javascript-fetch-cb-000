e87850d9d87a0139e14b053dfef1c09688c0adb5
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-11-26 14:21:48 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-fetch('https://api.github.com/user/repos')
+const token = 'YOUR_TOKEN_HERE';
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
   .then(res => res.json())
   .then(json => console.log(json));
 
